5803be545b3650be676c46a4e6f9d94091838168
Issei.Muratani <muurann@gmail.com>
2016-05-15 16:40:35 +0000

    model controller

--- app/controllers/mssages_controller.rb
+++ app/controllers/mssages_controller.rb
@@ -1,4 +1,19 @@
 class MssagesController < ApplicationController
   def index
+    @mssage = Mssage.new
+    @mssages = Mssage.all
   end
+  
+  def create
+    @mssage = Mssage.new(mssage_params)
+    @mssage.save
+    redirect_to root_path , notice:'メッセージを保存しました'
+  end
+  
+  private
+  def mssage_params
+    params.require(:mssage).permit(:name,:age,:body)
+  end
+  
+  ##ここまで
 end
